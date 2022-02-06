961522144b84ef1628d808852752670a19429b61
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2022-02-06 16:09:42 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Shoko KOBAYASHI on 2022/02/06.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
