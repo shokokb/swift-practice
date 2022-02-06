712a175d909ba0e99b328b20782d7b0dfea21db5
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2022-02-06 18:43:08 +0900

    LandmarkList

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Shoko KOBAYASHI on 2022/02/06.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks, id: \.id) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
